a1c4cc0e046d677d6bf758d0cd141ddde76fb2b7
Christian Binder <Bendi87@gmx.ch>
2015-09-15 20:29:31 +0200

    modified bitconverter

--- application/KlimaLogProApp/KlimaLoggBitConverter/bitconverter.h
+++ application/KlimaLogProApp/KlimaLoggBitConverter/bitconverter.h
@@ -3,14 +3,37 @@
 
 #include "bitconverter_global.h"
 
+struct SensorData
+{
+    double Temperature;
+    bool TempValid;
+    double Humidity;
+    bool HumValid;
+};
+
+struct Record
+{
+    long Timestamp;
+    bool TimeValid;
+    SensorData SensorDatas[9];
+};
+
+
 class BITCONVERTERSHARED_EXPORT BitConverter
 {
 
 public:
     BitConverter();
-    static double ConvertTemperature(short data, bool highByteFull);
-    static int ConvertHumidity(char data);
-   // static int ConvertTimestamp(char )
+    static bool ConvertTemperature(short data, bool highByteFull, double *value);
+    static bool ConvertHumidity(char data, double *value);
+    static bool ConvertHistoryTimestamp(char *data, long *value);
+    static bool ConvertCurrentTimestamp(char *data, bool startOnHighNibble, long *value);
+
+    static Record GetSensorValuesFromHistoryData(char* frame, int index);
+    static Record GetSensorValuesFromCurrentData(char* frame);
+
+
+    //static void ConvertUSBFrame(char* data, long *timestamp, SesnorData *values);
 };
 
 #endif // BITCONVERTER_H
