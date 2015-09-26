3b30e409b39d131d80774a627b9ebc9136752ed8
Urs Suhner <urs.suhner@gmail.com>
2015-09-26 10:15:41 +0200

    humidity axis and proof-of-concept for variable ticks

--- Qt/KlimaLoggProOnBBB/mainwindow.cpp
+++ Qt/KlimaLoggProOnBBB/mainwindow.cpp
@@ -67,7 +67,7 @@ MainWindow::MainWindow(QWidget *parent) :
 
     //initialize plot
     makePlot();
-//    OnDrawPlot();
+    OnDrawPlot();
 
 
     m_AcquisitionThread->start();
@@ -153,31 +153,28 @@ void MainWindow::OnDrawPlot()
 
     //reconfigure axis
     ui->customPlot->xAxis->setRange(x1[0], x1[count-1]);
-    int tickStep = (int)m_kldatabase->GetTimeIntervall() / 4;
-    ui->customPlot->xAxis->setTickStep(tickStep);
 
     //redraw
     ui->customPlot->replot();
-
 }
 
 void MainWindow::makePlot()
 {
     // create and configure plottables:
     QCPGraph *graph1 = ui->customPlot->addGraph();
-    QCPGraph *graph2 = ui->customPlot->addGraph();
+    QCPGraph *graph2 = ui->customPlot->addGraph(ui->customPlot->xAxis, ui->customPlot->yAxis2);
     QCPGraph *graph3 = ui->customPlot->addGraph();
-    QCPGraph *graph4 = ui->customPlot->addGraph();
+    QCPGraph *graph4 = ui->customPlot->addGraph(ui->customPlot->xAxis, ui->customPlot->yAxis2);
 
     graph1->setPen(QPen(QColor(Qt::yellow), 4));
-    graph2->setPen(QPen(QColor(0, 172, 172), 4));
+    graph2->setPen(QPen(QColor(0, 80, 80), 4));
     graph3->setPen(QPen(QColor(255, 0, 0), 4));
-    graph4->setPen(QPen(QColor(0, 0, 255), 4));
+    graph4->setPen(QPen(QColor(0, 0, 120), 4));
 
-    graph1->setName("Innen Temperatur");
-    graph2->setName("Innen Luftfeuchtigkeit");
-    graph3->setName("Aussen Temperatur");
-    graph4->setName("Aussen Luftfeuchtigkeit");
+    graph1->setName("innen Temperatur");
+    graph2->setName("innen Luftfeuchtigkeit");
+    graph3->setName("aussen Temperatur");
+    graph4->setName("aussen Luftfeuchtigkeit");
 
     ui->customPlot->legend->setVisible(true);
 
@@ -195,35 +192,56 @@ void MainWindow::makePlot()
     graph1->setLayer("abovemain");
     ui->customPlot->xAxis->grid()->setLayer("belowmain");
     ui->customPlot->yAxis->grid()->setLayer("belowmain");
+    ui->customPlot->yAxis2->grid()->setLayer("belowmain");
 
     // set some pens, brushes and backgrounds:
     ui->customPlot->xAxis->setBasePen(QPen(Qt::white, 1));
     ui->customPlot->yAxis->setBasePen(QPen(Qt::white, 1));
+    ui->customPlot->yAxis2->setBasePen(QPen(Qt::white, 1));
+
     ui->customPlot->xAxis->setTickPen(QPen(Qt::white, 1));
     ui->customPlot->yAxis->setTickPen(QPen(Qt::white, 1));
+    ui->customPlot->yAxis2->setTickPen(QPen(Qt::white, 1));
+
     ui->customPlot->xAxis->setSubTickPen(QPen(Qt::white, 1));
     ui->customPlot->yAxis->setSubTickPen(QPen(Qt::white, 1));
+    ui->customPlot->yAxis2->setSubTickPen(QPen(Qt::white, 1));
+
     ui->customPlot->xAxis->setTickLabelColor(Qt::white);
     ui->customPlot->yAxis->setTickLabelColor(Qt::white);
+    ui->customPlot->yAxis2->setTickLabelColor(Qt::white);
 
     ui->customPlot->xAxis->setTickLabelType(QCPAxis::ltDateTime);
-    ui->customPlot->xAxis->setDateTimeFormat("dd.MM.yyyy hh:mm");
-    ui->customPlot->xAxis->setAutoTickStep(false);
-//    ui->customPlot->xAxis->setTickStep(6);
 
+    ui->customPlot->xAxis->setAutoTicks(false);
+
+    ui->customPlot->xAxis->setAutoTickStep(false);
     ui->customPlot->yAxis->setAutoTickStep(false);
+    ui->customPlot->yAxis2->setAutoTickStep(false);
+
     ui->customPlot->yAxis->setTickStep(5);
+    ui->customPlot->yAxis2->setTickStep(5);
 
     ui->customPlot->xAxis->grid()->setPen(QPen(QColor(140, 140, 140), 1, Qt::DotLine));
     ui->customPlot->yAxis->grid()->setPen(QPen(QColor(140, 140, 140), 1, Qt::DotLine));
+    ui->customPlot->yAxis2->grid()->setPen(QPen(QColor(140, 140, 140), 1, Qt::DotLine));
+
     ui->customPlot->xAxis->grid()->setSubGridPen(QPen(QColor(80, 80, 80), 1, Qt::DotLine));
     ui->customPlot->yAxis->grid()->setSubGridPen(QPen(QColor(80, 80, 80), 1, Qt::DotLine));
+    ui->customPlot->yAxis2->grid()->setSubGridPen(QPen(QColor(80, 80, 80), 1, Qt::DotLine));
+
     ui->customPlot->xAxis->grid()->setSubGridVisible(true);
     ui->customPlot->yAxis->grid()->setSubGridVisible(true);
+    ui->customPlot->yAxis2->grid()->setSubGridVisible(true);
+
     ui->customPlot->xAxis->grid()->setZeroLinePen(Qt::NoPen);
     ui->customPlot->yAxis->grid()->setZeroLinePen(Qt::NoPen);
+    ui->customPlot->yAxis2->grid()->setZeroLinePen(Qt::NoPen);
+
     ui->customPlot->xAxis->setUpperEnding(QCPLineEnding::esSpikeArrow);
     ui->customPlot->yAxis->setUpperEnding(QCPLineEnding::esSpikeArrow);
+    ui->customPlot->yAxis2->setUpperEnding(QCPLineEnding::esSpikeArrow);
+
     QLinearGradient plotGradient;
     plotGradient.setStart(0, 0);
     plotGradient.setFinalStop(0, 350);
@@ -236,22 +254,45 @@ void MainWindow::makePlot()
     axisRectGradient.setColorAt(0, QColor(30, 30, 30));
     axisRectGradient.setColorAt(1, QColor(30, 30, 30));
     ui->customPlot->axisRect()->setBackground(axisRectGradient);
-    ui->customPlot->yAxis->setRange(10, 70);
+    ui->customPlot->yAxis->setRange(10, 30);
+    ui->customPlot->yAxis2->setRange(35, 70);
+
     ui->customPlot->xAxis->setLabelColor(Qt::white);
     ui->customPlot->yAxis->setLabelColor(Qt::white);
-    ui->customPlot->xAxis->setLabel("Zeit");
-    ui->customPlot->yAxis->setLabel("Temperatur");
+    ui->customPlot->yAxis2->setLabelColor(Qt::white);
 
-//    ui->customPlot->yAxis2->setVisible(true);
-//    ui->customPlot->yAxis2->setRange(0, 100);
-//    ui->customPlot->yAxis2->setLabel("Luftfeuchtigkeit");
+    ui->customPlot->xAxis->setLabel("Zeit");
+    ui->customPlot->yAxis->setLabel("Temperatur [°C]");
+    ui->customPlot->yAxis2->setLabel("relative Luftfeuchtigkeit [%]");
+
+    QVector<double> xAxisTicks;
+    xAxisTicks << 1442613600 << 1442700000 << 1442786400 << 1442872800 << 1442959200 << 1443045600 << 1443132000 << 1443218400 << 1443304800;
+    ui->customPlot->xAxis->setTickVector(xAxisTicks);
+    ui->customPlot->xAxis->setSubTickCount(3);
+    ui->customPlot->xAxis->setDateTimeFormat("dd.MM.yy");
+
+    ui->customPlot->yAxis2->setVisible(true);
 }
 
 void MainWindow::selectShortTimespan()
 {
     qDebug() << "15 Minuten";
     m_kldatabase->SetTimeIntervall(TimeIntervall::SHORT);
-    emit DrawPlot();
+    QVector<double> xAxisTicks;
+    xAxisTicks << 1443243600 << 1443243600 +300  << 1443243600 +600 << 1443243600 +900 << 1443243600  +1200<< 1443243600  +1500 << 1443243600  +1800
+               << 1443243600 +2100  << 1443243600 +2400 << 1443243600 +2700 << 1443243600 +3000 << 1443243600 +3300 << 1443243600 +3600
+               << 1443243600 +3600 << 1443243600 +300 +3600 << 1443243600 +600 +3600<< 1443243600 +900 +3600<< 1443243600  +1200+3600<< 1443243600  +1500 +3600<< 1443243600  +1800+3600
+               << 1443243600 +2100 +3600 << 1443243600 +2400 +3600<< 1443243600 +2700 +3600<< 1443243600 +3000 +3600<< 1443243600 +3300 +3600<< 1443243600 +3600+3600
+               << 1443243600 +2*3600<< 1443243600 +300 +2*3600 << 1443243600 +600 +2*3600<< 1443243600 +900 +2*3600<< 1443243600  +1200+2*3600<< 1443243600  +1500 +2*3600<< 1443243600  +1800+2*3600
+               << 1443243600 +2100+2*3600  << 1443243600 +2400 +2*3600<< 1443243600 +2700+2*3600 << 1443243600 +3000 +2*3600<< 1443243600 +3300 +2*3600<< 1443243600 +3600     +2*3600
+                  ;
+    ui->customPlot->xAxis->setTickVector(xAxisTicks);
+    ui->customPlot->xAxis->setSubTickCount(4);
+    ui->customPlot->xAxis->setDateTimeFormat("dd.MM.yy hh:mm");
+
+  //  OnDrawPlot();
+    // emit DrawPlot();
+        ui->customPlot->replot();
 }
 
 
@@ -259,14 +300,32 @@ void MainWindow::selectMediumTimespan()
 {
     qDebug() << "24 Stunden";
     m_kldatabase->SetTimeIntervall(TimeIntervall::MEDIUM);
-    emit DrawPlot();
+    QVector<double> xAxisTicks;
+    xAxisTicks << 1443132000 << 1443132000 +4*3600 << 1443132000 +8*3600 << 1443132000 +12*3600 << 1443132000  +16*3600<< 1443132000  +20*3600 << 1443132000  +24*3600
+               << 1443132000 +28*3600  << 1443132000 +32*3600 << 1443132000 +36*3600 << 1443132000 +40*3600 << 1443132000 +44*3600 << 1443132000 +48*3600 ;
+    ui->customPlot->xAxis->setTickVector(xAxisTicks);
+    ui->customPlot->xAxis->setSubTickCount(3);
+    ui->customPlot->xAxis->setDateTimeFormat("dd.MM.yy hh:mm");
+
+//    OnDrawPlot();
+    // emit DrawPlot();
+        ui->customPlot->replot();
 }
 
 void MainWindow::selectLongTimespan()
 {
     qDebug() << "7 Tage";
     m_kldatabase->SetTimeIntervall(TimeIntervall::LONG);
-    emit DrawPlot();
+
+    QVector<double> xAxisTicks;
+    xAxisTicks << 1442613600 << 1442700000 << 1442786400 << 1442872800 << 1442959200 << 1443045600 << 1443132000 << 1443218400 << 1443304800;
+    ui->customPlot->xAxis->setTickVector(xAxisTicks);
+    ui->customPlot->xAxis->setSubTickCount(3);
+    ui->customPlot->xAxis->setDateTimeFormat("dd.MM.yy");
+
+ //   OnDrawPlot();
+    // emit DrawPlot();
+        ui->customPlot->replot();
 }
 
 void MainWindow::newData() {
