3a330d59af8aabe6ddb4efa3ada5f8b728bf24b6
Urs Suhner <urs.suhner@gmail.com>
2015-09-27 09:29:01 +0200

    workaround sommertime/wintertime

--- Qt/KlimaLoggProOnBBB/definitions.h
+++ Qt/KlimaLoggProOnBBB/definitions.h
@@ -3,7 +3,7 @@
 
 
 #define SENSOR "/dev/kl0"
-#define TIME_BASIS 1420066800
+#define TIME_BASIS 1430431200
 #define KLIMALOGG_DATABASE "/usr/local/bin/database/KlimaLoggPro.sdb"
 
 struct SensorData
